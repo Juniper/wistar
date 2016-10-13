dda9e0237782bd6728d6544a11f2288d42700a43
Nathan Embery <nembery@gmail.com>
2016-10-13 16:58:18 -0400

    junos space vm fix

--- common/static/js/vm_types/space.js
+++ common/static/js/vm_types/space.js
@@ -1,6 +1,8 @@
 draw2d.shape.node.space = draw2d.shape.node.linux.extend({
     NAME: "draw2d.shape.node.space",
     CLOUD_INIT_SUPPORT: false,
+    MANAGEMENT_INTERFACE_PREFIX: "eth",
+    MANAGEMENT_INTERFACE_INDEX: -1,
     VCPU: 4,
     VRAM: 16384,
     ICON_WIDTH: 50,
