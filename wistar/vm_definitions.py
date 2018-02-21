5172bed757952b873a8ba62f404de934e96a5627
Nathan Embery <nembery@juniper.net>
2018-02-21 13:52:49 -0500

    split out vm_image_types from configuration.py

---
+++ wistar/vm_definitions.py
@@ -0,0 +1,87 @@
+vm_image_types = [
+    {
+        "name": "blank",
+        "description": "Blank",
+        "js": "draw2d.shape.node.generic",
+    },
+    {
+        "name": "linux",
+        "description": "Linux",
+        "js": "draw2d.shape.node.linux",
+    },
+    {
+        "name": "ubuntu16",
+        "description": "Ubuntu 16",
+        "js": "draw2d.shape.node.ubuntu16",
+    },
+    {
+        "name": "junos_vmx",
+        "description": "Junos vMX 14.x",
+        "js": "draw2d.shape.node.vmx",
+    },
+    {
+        "name": "junos_vre",
+        "description": "Junos vMX RE Latest",
+        "js": "draw2d.shape.node.vre",
+    },
+    {
+        "name": "junos_vre_15",
+        "description": "Junos vMX RE 15.x",
+        "js": "draw2d.shape.node.vre_15",
+    },
+    {
+        "name": "junos_vre_15",
+        "description": "Junos vMX RE 16.x",
+        "js": "draw2d.shape.node.vre_15",
+    },
+    {
+        "name": "junos_vpfe",
+        "description": "Junos vMX vFPC",
+        "js": "draw2d.shape.node.vpfe",
+    },
+    {
+        "name": "junos_vpfe_haswell",
+        "description": "Junos vMX vFPC (Haswell)",
+        "js": "draw2d.shape.node.vpfe_haswell",
+    },
+    {
+        "name": "junos_vqfx_re",
+        "description": "Junos vQFX RE",
+        "js": "draw2d.shape.node.vqfxRe",
+    },
+    {
+        "name": "junos_riot",
+        "description": "Junos vMX RIOT",
+        "js": "draw2d.shape.node.vriot",
+    },
+    {
+        "name": "junos_vrr",
+        "description": "Junos Virtual Route Reflector",
+        "js": "draw2d.shape.node.vrr",
+    },
+    {
+        "name": "junos_vqfx_cosim",
+        "description": "Junos vQFX PFE",
+        "js": "draw2d.shape.node.vqfxCosim",
+    },
+    {
+        "name": "generic",
+        "description": "Other",
+        "js": "draw2d.shape.node.generic",
+    },
+    {
+        "name": "junos_vsrx",
+        "description": "Junos vSRX",
+        "js": "draw2d.shape.node.vsrx",
+    },
+    {
+        "name": "junos_vmx_hdd",
+        "description": "Junos vMX HDD",
+        "js": "draw2d.shape.node.generic",
+    },
+        {
+        "name": "space",
+        "description": "Junos Space",
+        "js": "draw2d.shape.node.space",
+    }
+]
